49a4ad1ce2d79c64437bc53c0f37a259fd526a40
Nayasha24 <114497250+Nayasha24@users.noreply.github.com>
2024-06-08 14:42:32 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Index of an Extra Element/index-of-an-extra-element.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int findExtra(int n, int arr1[], int arr2[]) {
+        unordered_set<int> ar2;
+        for (int i=0; i<n-1; i++){
+            ar2.insert(arr2[i]);
+        }
+        
+        for (int i=0; i<n; i++){
+            if (ar2.find(arr1[i])==ar2.end()){
+                
+                return i;
+            }
+       
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr1[n], arr2[n - 1];
+        for (int i = 0; i < n; i++) {
+            cin >> arr1[i];
+        }
+        for (int i = 0; i < n - 1; i++) {
+            cin >> arr2[i];
+        }
+        Solution obj;
+        cout << obj.findExtra(n, arr1, arr2) << endl;
+    }
+}
+// } Driver Code Ends
